2722872083a906fb12f5d3ba379d35701243b97a
Benjamin Dengler <benjamin.dengler@gmail.com>
2017-10-11 08:21:48 +0200

    Moved status led code from keymap to main board.

--- keyboards/ps2avrGB/ps2avrGB.c
+++ keyboards/ps2avrGB/ps2avrGB.c
@@ -43,3 +43,23 @@ __attribute__ ((weak))
 void matrix_scan_user(void) {
     rgblight_task();
 }
+
+void led_set_user(uint8_t usb_led) { 
+    if (usb_led & (1 << USB_LED_NUM_LOCK)) {
+        DDRD &= ~(1 << 0); PORTD &= ~(1 << 0);
+    } else {
+        DDRD |= (1 << 0); PORTD |= (1 << 0); 
+    }
+        
+    if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+        DDRD &= ~(1 << 1); PORTD &= ~(1 << 1);
+    } else {
+        DDRD |= (1 << 1); PORTD |= (1 << 1);
+    }
+        
+    if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
+        DDRD &= ~(1 << 6); PORTD &= ~(1 << 6);      
+    } else {
+        DDRD |= (1 << 6); PORTD |= (1 << 6);     
+    }
+}
